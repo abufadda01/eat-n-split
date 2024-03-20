0497fa1983780d4823f30de84833e6c0148b022a
abufadda01 <laithfadda4@gmail.com>
2024-03-20 15:18:57 +0300

    complete add friend form

--- src/App.js
+++ src/App.js
@@ -5,35 +5,38 @@ import FriendList from './components/FriendList';
 import SplitBillForm from './components/SplitBillForm';
 
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+
+
 function App() {
 
   const [openAddFriendForm , setOpenAddFriendForm] = useState(false)
+  const [friends , setFriends] = useState(initialFriends)
 
   const handleFormOpen = () => {
     setOpenAddFriendForm(prev => !prev)
   }
 
-
-  const initialFriends = [
-    {
-      id: 118836,
-      name: "Clark",
-      image: "https://i.pravatar.cc/48?u=118836",
-      balance: -7,
-    },
-    {
-      id: 933372,
-      name: "Sarah",
-      image: "https://i.pravatar.cc/48?u=933372",
-      balance: 20,
-    },
-    {
-      id: 499476,
-      name: "Anthony",
-      image: "https://i.pravatar.cc/48?u=499476",
-      balance: 0,
-    },
-  ];
 
 
   return (
@@ -41,8 +44,8 @@ function App() {
 
       <div className='sidebar'>
       
-        <FriendList friends={initialFriends} />
-        {openAddFriendForm && <AddFriendForm />}
+        <FriendList friends={friends} />
+        {openAddFriendForm && <AddFriendForm setFriends={setFriends} />}
 
         {/* send a fun as a props then set it as an event fun in the children component */}
         <Button handleFormOpen={handleFormOpen}>{openAddFriendForm ? "Close" : "Add Friend"}</Button>
--- src/components/AddFriendForm.jsx
+++ src/components/AddFriendForm.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
-const AddFriendForm = () => {
+
+const AddFriendForm = ({setFriends}) => {
+
+    const [formData , setFormData] = useState({
+        name : "" ,
+        image : "https://i.pravatar.cc/48"
+    })
+
+
+    const handleChange = (e) => {
+        setFormData({...formData , [e.target.name] : e.target.value})
+    }
+
+
+    const handleSubmlt = (e) => {
+        
+        e.preventDefault()
+
+        const id = crypto.randomUUID() 
+        
+        const newFriend = {
+            id , 
+            name : formData.name ,
+            image : `${formData.image}?=${id}` ,
+            balance : 0 
+        }
+
+        if(!formData.name || !formData.image) return
+
+        setFriends(prev => [...prev , newFriend])
+
+        setFormData({name : "" , image : "https://i.pravatar.cc/48"})
+
+    }
+
+
   return (
-    <form className='form-add-friend'>
+    <form onSubmit={handleSubmlt} className='form-add-friend'>
 
         <label>🧑‍🤝‍🧑Friend Name</label>
-        <input type="text" />
+        <input onChange={handleChange} name='name' value={formData.name}  type="text" />
 
         <label>🖼️Image Url</label>
-        <input type="text" />
+        <input onChange={handleChange} name='image' value={formData.image} type="text" />
         
         <Button>Add</Button>
 
